8d893f0a03455e1dc6134499cbd8a67caa25cb7b
Kenneth <kennethkskristensen@hotmail.com>
2021-03-12 14:38:01 +0100

    added more views, and a nav menu + routes to these

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/books.pug', function(req, res, next) {
+  res.render('books', { title: 'Books page' });
+});
+router.get('/persons.pug', function(req, res, next) {
+  res.render('persons', { title: 'User registration page' });
+});
 
 module.exports = router;
