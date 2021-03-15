79f812f902ee7a95282d598ab459911041b33a96
Kenneth <kennethkskristensen@hotmail.com>
2021-03-15 11:32:30 +0100

    Changed router functionality for viewbooks.pug

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Library Front Page' });
 });
-router.get('/books.pug', function(req, res, next) {
-  res.render('books', { title: 'Books page' });
+router.get('/viewbooks.pug', function(req, res, next) {
+  res.render('viewbooks', { title: 'View books in library' });
+  const dbname = "books";
+  const findDB = `mongodb://localhost:27017/${dbname}`;
+  const conparam = { useNewUrlParser: true, useUnifiedTopology: true };
+  mongoose.connect(findDB, conparam);
+  const db = mongoose.connection;
+  db.once("open", function() {
+    console.log("Connected to server by mongoose");
+  });
 });
-router.get('/persons.pug', function(req, res, next) {
-  res.render('persons', { title: 'User registration page' });
+router.get('/addbooks.pug', function(req, res, next) {
+  res.render('addbooks', { title: 'Add books to library' });
 });
+// router.get('/persons.pug', function(req, res, next) {
+//   res.render('persons', { title: 'User registration page' });
+// });
 
 module.exports = router;
