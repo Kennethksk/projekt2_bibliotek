04b1a7e7129baab1199629acca9da61176f3c4e7
Kenneth <kennethkskristensen@hotmail.com>
2021-03-17 09:28:22 +0100

    changes to viewbooks files

--- models/books.js
+++ models/books.js
@@ -12,4 +12,4 @@ const booksSchema = mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model("Book", booksSchema, 'books');    // 1. parameter er modul navn, 2. parameter er schema og 3. parameter er kollektionsnavn(books collection fra databasen)
+module.exports = mongoose.model("Book", booksSchema, 'books');    // 1. parameter er modulets navn, 2. parameter er schema og 3. parameter er kollektionsnavn(books collection fra databasen)
